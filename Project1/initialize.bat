2620e4bf9a278742d8f318fff9ccbf7a2e2116ab
jsmit496 <jsmit496@stumail.jccc.edu>
2018-02-06 19:19:12 -0600

    created layout for project

--- Project1/initialize.bat
+++ Project1/initialize.bat
@@ -1,17 +1,34 @@
 @echo off
 
-set initialize=%1
-set usernameemail=%2
-set display1=%3
-set 2usernameemail=%4
-set display2=%5
+REM For the initializer (this document) I want the user to write:
+REM (initialize <repository>) to simply create the spot on the computer this might also involve pulling branches
+REM (initialize username <username> email <email>) to configure the environment
+REM think about how to "check out" a development branch
 
-IF "%1"=="initialize" (
-	echo pizza
-	IF "%2"=="username" (
-		echo username: %3
-		
-	) ELSE IF "%2"=="email" (
-		echo email: %3
-	)
-)
+REM For the updater (different document) I want the user to write:
+REM (update) to pull commits from the remote repository and merge them into the local
+REM if their local is not commited then this tool cannot update unless they commit or revert the merge focuses on "theirs"
+
+REM For the Commiter (different document) I want the user to write:
+REM (commit) to commit and push all of their changes
+
+REM For the Reverter (different document) I want the user to write:
+REM (revert) to discard all changes since the last commit
+
+set usernameemail=%1
+set display1=%2
+set 2usernameemail=%3
+set display2=%4
+
+REM IF "%1"=="username" (
+REM	 echo username: %2
+REM	 IF "%3"=="email" (
+REM	 	echo email: %4
+REM	 )
+REM ) ELSE IF "%1"=="email" (
+REM	 echo email: %2
+REM	 IF "%3"=="username" (
+REM	 	echo username: %4
+REM	 )
+REM )
+
