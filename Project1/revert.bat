7b7b73cf5b1a07ae007e93b5a3d5ed466531e7ca
jsmit496 <jsmit496@stumail.jccc.edu>
2018-02-13 16:37:23 -0600

    completed revet for project

--- Project1/revert.bat
+++ Project1/revert.bat
@@ -6,12 +6,15 @@ REM git-scm.com
 REM For the Reverter (different document) I want the user to write:
 REM (revert) to discard all changes since the last commit
 
-set usernameemail=%1
-set display1=%2
-set 2usernameemail=%3
-set display2=%4
+setlocal
 
-call git
+set /P yesOrNo=Are you sure you want to reset to the last commit (Y/N)?
+IF %yesOrNo%==Y	(
+	call git reset --hard
+	endlocal
+)
+
+
 
 
 
