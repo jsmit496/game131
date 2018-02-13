f49faa8d3c502cae4a6f0b874f51315cf474b81c
jsmit496 <jsmit496@stumail.jccc.edu>
2018-02-13 11:48:35 -0600

    testing commit

--- Project1/commit.bat
+++ Project1/commit.bat
@@ -6,12 +6,10 @@ REM git-scm.com
 REM For the Committer (different document) I want the user to write:
 REM (commit) to commit and push all of their changes
 
-set usernameemail=%1
-set display1=%2
-set 2usernameemail=%3
-set display2=%4
+set commitMessage=%1
 
-call git.exe commit -a
+call git add -A
+call git commit -m %1
 
 
 
