71e406a70f8f747a32783e939b25e8bfff1e56f7
jsmit496 <jsmit496@stumail.jccc.edu>
2018-02-13 11:28:42 -0600

    fixing some deleted stuff

--- Project1/initialize.bat
+++ Project1/initialize.bat
@@ -8,22 +8,21 @@ REM (initialize <repository>) to simply create the spot on the computer this mig
 REM (initialize username <username> email <email>) to configure the environment
 REM think about how to "check out" a development branch
 
-REM For the updater (different document) I want the user to write:
-REM (update) to pull commits from the remote repository and merge them into the local
-REM if their local is not committed then this tool cannot update unless they commit or revert the merge focuses on "theirs"
-
-REM For the Committer (different document) I want the user to write:
-REM (commit) to commit and push all of their changes
-
-REM For the Reverter (different document) I want the user to write:
-REM (revert) to discard all changes since the last commit
-
-set usernameemail=%1
+set repository=%1
 set display1=%2
 set 2usernameemail=%3
 set display2=%4
 
-call git.exe clone %1
+REM basic get repository and pull branches. (initialize repository <repository>)
+If %1==repository (
+	call git clone %1
+	call git fetch --all
+	call git pull --all
+	call git branch Development
+	call git checkout Development
+)
+
+
 
 
 
---
+++ Project1/revert.bat
@@ -0,0 +1,17 @@
+@echo off
+
+REM ss64.com/nt
+REM git-scm.com
+
+REM For the Reverter (different document) I want the user to write:
+REM (revert) to discard all changes since the last commit
+
+set usernameemail=%1
+set display1=%2
+set 2usernameemail=%3
+set display2=%4
+
+call git.exe clone %1
+
+
+
