40211df4049c8d66fcdca413456051576a3cdf1d
jsmit496 <jsmit496@stumail.jccc.edu>
2018-02-13 17:09:22 -0600

    added in the help information

--- Project1/help.bat
+++ Project1/help.bat
@@ -3,16 +3,13 @@
 REM ss64.com/nt
 REM git-scm.com
 
-REM For the updater (different document) I want the user to write:
-REM (update) to pull commits from the remote repository and merge them into the local
-REM if their local is not committed then this tool cannot update unless they commit or revert the merge focuses on "theirs"
+echo initialize repository (repository)
+echo initialize name (name) email (email)
+echo initialize email (email) name (name)
+echo commit ("message")
+echo revert
+echo update
 
-set usernameemail=%1
-set display1=%2
-set 2usernameemail=%3
-set display2=%4
-
-call git.exe 
 
 
 
