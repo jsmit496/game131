2ededbde7d00a72b49492f8fd49dc54364032615
jsmit496 <jsmit496@stumail.jccc.edu>
2018-03-29 18:56:37 -0500

    editorWindow Demo

---
+++ editorWindow/Assets/Editor/MenuItems.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+public class MenuItems
+{
+
+    [MenuItem("Tools/Level Creator/Show Level Editor")]
+	private static void ShowLevelEditor()
+    {
+        LevelEditorWindow.ShowWindow();
+    }
+}
