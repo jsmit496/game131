169e378ddf3e135dca5454bf9e13fa2ec73acb28
jsmit496 <jsmit496@stumail.jccc.edu>
2018-03-29 19:18:01 -0500

    Setup for Project2

---
+++ Project2/Project2Start/Assets/Editor/LevelEditorWindow.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+//Goal: Add new obstacles and targets within a custom Editor Window
+//Additions: ???
+public class LevelEditorWindow : EditorWindow
+{
+    private static LevelEditorWindow customEditor;
+
+    public static void ShowWindow()
+    {
+        customEditor = EditorWindow.GetWindow<LevelEditorWindow>();
+        customEditor.titleContent = new GUIContent("Level Editor");
+    }
+
+    private void OnGUI()
+    {
+        
+    }
+
+}
---
+++ Project2/Project2Start/Assets/Editor/MenuItems.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+public class MenuItems
+{
+    [MenuItem("Tools/Level Editor Window")]
+    private static void ShowLevelEditor()
+    {
+        LevelEditorWindow.ShowWindow();
+    }
+}
