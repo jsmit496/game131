20e5e9df2533db04f0a232882d7191413cdf107a
jsmit496 <jsmit496@stumail.jccc.edu>
2018-04-10 18:22:18 -0500

    learning event on gui stuff

---
+++ sceneGUIDemo/Assets/Editor/EventCatcherEditor.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(EventCatcher))]
+public class EventCatcherEditor : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        base.OnInspectorGUI();
+    }
+
+    private void OnSceneGUI()
+    {
+        Event currentEvent = Event.current;
+
+        switch (currentEvent.type)
+        {
+            case EventType.KeyDown:
+                if (currentEvent.keyCode != KeyCode.None)
+                {
+                    UnityEngine.MonoBehaviour.print("Key down: " + currentEvent.keyCode);
+                }
+                currentEvent.Use();
+                break;
+            case EventType.KeyUp:
+                UnityEngine.MonoBehaviour.print("Key up: " + currentEvent.keyCode);
+                currentEvent.Use();
+                break;
+        }
+
+    }
+
+}
