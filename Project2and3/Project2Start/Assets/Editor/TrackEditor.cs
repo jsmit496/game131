86fd1cff4d26dbdd02374f993556d3b07a1a81d3
jsmit496 <jsmit496@stumail.jccc.edu>
2018-04-24 19:19:51 -0500

    Some Edits

---
+++ Project2and3/Project2Start/Assets/Editor/TrackEditor.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(Track))]
+public class TrackEditor : Editor
+{
+    private Track _myTarget;
+    private float defaultTrackSpeed;
+    private float trackSpeed;
+    private bool setStuff = false;
+
+    private void OnEnable()
+    {
+        _myTarget = (Track)this.target;
+    }
+
+    public override void OnInspectorGUI()
+    {
+        if (!setStuff)
+        {
+            trackSpeed = defaultTrackSpeed;
+            setStuff = true;
+        }
+
+        defaultTrackSpeed = EditorGUILayout.FloatField("Default Track Speed", defaultTrackSpeed);
+        trackSpeed = EditorGUILayout.FloatField("Track Speed", trackSpeed);
+    }
+}
