f65ccb1e78d64c6a50f4167d9b37e541bede8ce7
jsmit496 <jsmit496@stumail.jccc.edu>
2018-04-30 20:48:55 -0500

    work on project3

--- Project2and3/Project2Start/Assets/Editor/TrackEditor.cs
+++ Project2and3/Project2Start/Assets/Editor/TrackEditor.cs
@@ -3,28 +3,38 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEditor;
 
-[CustomEditor(typeof(Track))]
+[CustomEditor(typeof(LinkObjects))]
 public class TrackEditor : Editor
 {
-    private Track _myTarget;
+    private LinkObjects _myTarget;
     private float defaultTrackSpeed;
-    private float trackSpeed;
-    private bool setStuff = false;
+    private float distancePercent;
+    private float distance;
+    private Vector3 difference;
+    private Vector3 positionForTrackObject;
 
     private void OnEnable()
     {
-        _myTarget = (Track)this.target;
+        _myTarget = (LinkObjects)this.target;
     }
 
     public override void OnInspectorGUI()
     {
-        if (!setStuff)
-        {
-            trackSpeed = defaultTrackSpeed;
-            setStuff = true;
-        }
-
         defaultTrackSpeed = EditorGUILayout.FloatField("Default Track Speed", defaultTrackSpeed);
-        trackSpeed = EditorGUILayout.FloatField("Track Speed", trackSpeed);
+
+        _myTarget.firstTrackPoint = (GameObject)EditorGUILayout.ObjectField("First Track Point", _myTarget.firstTrackPoint, typeof(GameObject), true);
+        _myTarget.secondTrackPoint = (GameObject)EditorGUILayout.ObjectField("Second Track Point", _myTarget.secondTrackPoint, typeof(GameObject), true);
+        _myTarget.linkedObject = (GameObject)EditorGUILayout.ObjectField("Object Linked To Track", _myTarget.linkedObject, typeof(GameObject), true);
+
+        //Calculate difference between points for object
+        EditorGUILayout.LabelField("Linked Object Position Slider");
+        _myTarget.distancePercent = EditorGUILayout.Slider(_myTarget.distancePercent, 0, 1);
+        _myTarget.distance = Vector3.Distance(_myTarget.firstTrackPoint.transform.position, _myTarget.secondTrackPoint.transform.position);
+        _myTarget.distance *= _myTarget.distancePercent;
+        _myTarget.difference = _myTarget.firstTrackPoint.transform.position - _myTarget.secondTrackPoint.transform.position;
+        _myTarget.difference = _myTarget.difference.normalized * _myTarget.distance;
+        _myTarget.distanceBetween = _myTarget.firstTrackPoint.transform.position - _myTarget.difference;
+
+        _myTarget.linkedObject.transform.position = _myTarget.distanceBetween;
     }
 }
