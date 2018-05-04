67a2b1e36890441f74fe2cd54bfe95abb2750a9e
jsmit496 <jsmit496@stumail.jccc.edu>
2018-05-03 20:31:46 -0500

    added button

--- Project2and3/Project2Start/Assets/Editor/LevelEditorWindow.cs
+++ Project2and3/Project2Start/Assets/Editor/LevelEditorWindow.cs
@@ -8,41 +8,42 @@ using System.Text;
 //Additions: ???
 public class LevelEditorWindow : EditorWindow
 {
-   private static LevelEditorWindow customEditor;
+    private static LevelEditorWindow customEditor;
 
-   public static void ShowWindow()
-   {
-      customEditor = EditorWindow.GetWindow<LevelEditorWindow>();
-      customEditor.titleContent = new GUIContent("Level Editor");
-   }
+    public static void ShowWindow()
+    {
+        customEditor = EditorWindow.GetWindow<LevelEditorWindow>();
+        customEditor.titleContent = new GUIContent("Level Editor");
+    }
 
-   private void OnGUI()
-   {
-      CreateEditorButton("Create Target", "Target");
-      CreateEditorButton("Create WallBase", "wallBase 1");
-      CreateEditorButton("Create Track", "Track");
-   }
+    private void OnGUI()
+    {
+        CreateEditorButton("Create Target", "Target");
+        CreateEditorButton("Create WallBase", "wallBase 1");
+        CreateEditorButton("Create Track", "Track");
+        CreateEditorButton("Create Crossbow", "crossbowRoot");
+    }
 
-   public static void CreateEditorButton(string buttonName, string objectName)
-   {
-      Vector2 defPos = new Vector2(0, 0);
+    public static void CreateEditorButton(string buttonName, string objectName)
+    {
+        Vector3 defPos = new Vector3(0, 0, 0);
 
-      if (GUILayout.Button(buttonName))
-      {
-         string[] objectGuids = AssetDatabase.FindAssets(objectName);
+        if (GUILayout.Button(buttonName))
+        {
+            string[] objectGuids = AssetDatabase.FindAssets(objectName);
 
-         if (objectGuids.Length > 0)
-         {
-            string trueObjectGuid = objectGuids[0];
+            if (objectGuids.Length > 0)
+            {
+                string trueObjectGuid = objectGuids[0];
 
-            string assetPath = AssetDatabase.GUIDToAssetPath(trueObjectGuid);
+                string assetPath = AssetDatabase.GUIDToAssetPath(trueObjectGuid);
 
-            GameObject objectTemplate = AssetDatabase.LoadAssetAtPath(assetPath, typeof(GameObject)) as GameObject;
-            GameObject newObject = GameObject.Instantiate(objectTemplate);
-            newObject.name = objectTemplate.name;
-            newObject.transform.position = defPos;
-         }
-      }
-   }
+                GameObject objectTemplate = AssetDatabase.LoadAssetAtPath(assetPath, typeof(GameObject)) as GameObject;
+                GameObject newObject = Instantiate(objectTemplate);
+                newObject.name = objectTemplate.name;
+                newObject.transform.position = defPos;
+            }
+        }
+    }
 
 }
--- Project2and3/Project2Start/Assets/Editor/TrackEditor.cs
+++ Project2and3/Project2Start/Assets/Editor/TrackEditor.cs
@@ -6,77 +6,86 @@ using UnityEditor;
 [CustomEditor(typeof(LinkObjects))]
 public class TrackEditor : Editor
 {
-   private LinkObjects _myTarget;
+    private LinkObjects _myTarget;
 
-   private SerializedProperty ftrackSpeed;
-   private SerializedProperty fdistancePercent;
-   private SerializedProperty fdistance;
-   private SerializedProperty v3difference;
-   private SerializedProperty v3positionForTrackObjects;
-   private SerializedProperty objfirstTrackPoint;
-   private SerializedProperty objsecondTrackPoint;
-   private SerializedProperty objlinkedObject;
-   private SerializedProperty firstPointArrow;
-   private SerializedProperty secondpointArrow;
+    private SerializedProperty ftrackSpeed;
+    private SerializedProperty fdistancePercent;
+    private SerializedProperty fdistance;
+    private SerializedProperty v3difference;
+    private SerializedProperty v3positionForTrackObjects;
+    private SerializedProperty objfirstTrackPoint;
+    private SerializedProperty objsecondTrackPoint;
+    private SerializedProperty objlinkedObject;
+    private SerializedProperty firstPointArrow;
+    private SerializedProperty secondpointArrow;
+    private SerializedProperty changeStartDirection;
 
-   private bool setup = false;
+    private bool setup = false;
 
-   private void OnEnable()
-   {
-      _myTarget = (LinkObjects)this.target;
+    private void OnEnable()
+    {
+        _myTarget = (LinkObjects)this.target;
 
-      ftrackSpeed = this.serializedObject.FindProperty("trackSpeed");
-      fdistancePercent = this.serializedObject.FindProperty("distancePercent");
-      fdistance = this.serializedObject.FindProperty("distance");
-      v3difference = this.serializedObject.FindProperty("difference");
-      v3positionForTrackObjects = this.serializedObject.FindProperty("distanceBetween");
-      objfirstTrackPoint = this.serializedObject.FindProperty("firstTrackPoint");
-      objsecondTrackPoint = this.serializedObject.FindProperty("secondTrackPoint");
-      objlinkedObject = this.serializedObject.FindProperty("linkedObject");
-      firstPointArrow = this.serializedObject.FindProperty("firstTrackArrow");
-      secondpointArrow = this.serializedObject.FindProperty("secondTrackArrow");
-   }
+        ftrackSpeed = this.serializedObject.FindProperty("trackSpeed");
+        fdistancePercent = this.serializedObject.FindProperty("distancePercent");
+        fdistance = this.serializedObject.FindProperty("distance");
+        v3difference = this.serializedObject.FindProperty("difference");
+        v3positionForTrackObjects = this.serializedObject.FindProperty("distanceBetween");
+        objfirstTrackPoint = this.serializedObject.FindProperty("firstTrackPoint");
+        objsecondTrackPoint = this.serializedObject.FindProperty("secondTrackPoint");
+        objlinkedObject = this.serializedObject.FindProperty("linkedObject");
+        firstPointArrow = this.serializedObject.FindProperty("firstTrackArrow");
+        secondpointArrow = this.serializedObject.FindProperty("secondTrackArrow");
+        changeStartDirection = this.serializedObject.FindProperty("moveTowardsFirstPoint");
+    }
 
-   public override void OnInspectorGUI()
-   {
-      if (!setup)
-      {
-         ftrackSpeed.floatValue = _myTarget.linkedObject.GetComponent<TrackSpeedDir>().defaultTrackSpeed;
-      }
+    public override void OnInspectorGUI()
+    {
+        firstPointArrow.objectReferenceValue = _myTarget.firstTrackArrow = (GameObject)EditorGUILayout.ObjectField("First Track Arrow", _myTarget.firstTrackArrow, typeof(GameObject), true);
+        secondpointArrow.objectReferenceValue = _myTarget.secondTrackArrow = (GameObject)EditorGUILayout.ObjectField("Second Track Arrow", _myTarget.secondTrackArrow, typeof(GameObject), true);
 
-      firstPointArrow.objectReferenceValue = _myTarget.firstTrackArrow = (GameObject)EditorGUILayout.ObjectField("First Track Arrow", _myTarget.firstTrackArrow, typeof(GameObject), true);
-      secondpointArrow.objectReferenceValue = _myTarget.secondTrackArrow = (GameObject)EditorGUILayout.ObjectField("Second Track Arrow", _myTarget.secondTrackArrow, typeof(GameObject), true);
+        if (_myTarget.moveTowardsFirstPoint)
+        {
+            _myTarget.firstTrackArrow.SetActive(true);
+            _myTarget.secondTrackArrow.SetActive(false);
+        }
+        else
+        {
+            _myTarget.firstTrackArrow.SetActive(false);
+            _myTarget.secondTrackArrow.SetActive(true);
+        }
 
-      if (_myTarget.moveTowardsFirstPoint)
-      {
-         _myTarget.firstTrackArrow.SetActive(true);
-         _myTarget.firstTrackArrow.SetActive(false);
-      }
-      else
-      {
-         _myTarget.firstTrackArrow.SetActive(false);
-         _myTarget.firstTrackArrow.SetActive(true);
-      }
+        ftrackSpeed.floatValue = _myTarget.trackSpeed = EditorGUILayout.FloatField("Default Track Speed", _myTarget.trackSpeed);
 
-      ftrackSpeed.floatValue = _myTarget.trackSpeed = EditorGUILayout.FloatField("Default Track Speed", _myTarget.trackSpeed);
+        objfirstTrackPoint.objectReferenceValue = _myTarget.firstTrackPoint = (GameObject)EditorGUILayout.ObjectField("First Track Point", _myTarget.firstTrackPoint, typeof(GameObject), true);
+        objsecondTrackPoint.objectReferenceValue = _myTarget.secondTrackPoint = (GameObject)EditorGUILayout.ObjectField("Second Track Point", _myTarget.secondTrackPoint, typeof(GameObject), true);
+        objlinkedObject.objectReferenceValue = _myTarget.linkedObject = (GameObject)EditorGUILayout.ObjectField("Object Linked To Track", _myTarget.linkedObject, typeof(GameObject), true);
 
-      objfirstTrackPoint.objectReferenceValue = _myTarget.firstTrackPoint = (GameObject)EditorGUILayout.ObjectField("First Track Point", _myTarget.firstTrackPoint, typeof(GameObject), true);
-      objsecondTrackPoint.objectReferenceValue = _myTarget.secondTrackPoint = (GameObject)EditorGUILayout.ObjectField("Second Track Point", _myTarget.secondTrackPoint, typeof(GameObject), true);
-      objlinkedObject.objectReferenceValue = _myTarget.linkedObject = (GameObject)EditorGUILayout.ObjectField("Object Linked To Track", _myTarget.linkedObject, typeof(GameObject), true);
+        if (!setup)
+        {
+            ftrackSpeed.floatValue = _myTarget.linkedObject.GetComponent<TrackSpeedDir>().defaultTrackSpeed;
+            setup = true;
+        }
 
-      //Calculate difference between points for object
-      EditorGUILayout.BeginHorizontal();
-      EditorGUILayout.LabelField("Linked Object Position Slider");
-      fdistancePercent.floatValue = _myTarget.distancePercent = EditorGUILayout.Slider(_myTarget.distancePercent, 0, 1);
-      EditorGUILayout.EndHorizontal();
-      fdistance.floatValue = _myTarget.distance = Vector3.Distance(_myTarget.firstTrackPoint.transform.position, _myTarget.secondTrackPoint.transform.position);
-      fdistance.floatValue = _myTarget.distance *= _myTarget.distancePercent;
-      v3difference.vector3Value = _myTarget.difference = _myTarget.firstTrackPoint.transform.position - _myTarget.secondTrackPoint.transform.position;
-      v3difference.vector3Value = _myTarget.difference = _myTarget.difference.normalized * _myTarget.distance;
-      v3positionForTrackObjects.vector3Value = _myTarget.distanceBetween = _myTarget.firstTrackPoint.transform.position - _myTarget.difference;
+        bool changeDir = GUILayout.Button("Change Start Direction");
+        if (changeDir)
+        {
+            changeStartDirection.boolValue = _myTarget.moveTowardsFirstPoint = !_myTarget.moveTowardsFirstPoint;
+        }
 
-      objlinkedObject.vector3Value = _myTarget.linkedObject.transform.position = _myTarget.distanceBetween;
+        //Calculate difference between points for object
+        EditorGUILayout.BeginHorizontal();
+        EditorGUILayout.LabelField("Linked Object Position Slider");
+        fdistancePercent.floatValue = _myTarget.distancePercent = EditorGUILayout.Slider(_myTarget.distancePercent, 0, 1);
+        EditorGUILayout.EndHorizontal();
+        fdistance.floatValue = _myTarget.distance = Vector3.Distance(_myTarget.firstTrackPoint.transform.position, _myTarget.secondTrackPoint.transform.position);
+        fdistance.floatValue = _myTarget.distance *= _myTarget.distancePercent;
+        v3difference.vector3Value = _myTarget.difference = _myTarget.firstTrackPoint.transform.position - _myTarget.secondTrackPoint.transform.position;
+        v3difference.vector3Value = _myTarget.difference = _myTarget.difference.normalized * _myTarget.distance;
+        v3positionForTrackObjects.vector3Value = _myTarget.distanceBetween = _myTarget.firstTrackPoint.transform.position - _myTarget.difference;
 
-      this.serializedObject.ApplyModifiedProperties();
-   }
+        _myTarget.linkedObject.transform.position = v3positionForTrackObjects.vector3Value;
+
+        this.serializedObject.ApplyModifiedProperties();
+    }
 }
